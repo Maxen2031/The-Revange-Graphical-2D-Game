851275ab6616017ff8ba6f8d538f8d7c5fe6c24c
Arda Yildirim <ardayildirim05@icloud.com>
2022-10-20 21:46:54 +0200

    Version: 0.1

--- src/Screen.java
+++ src/Screen.java
@@ -1,19 +1,23 @@
 
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Screen extends JFrame {
     public static int sizeX = 800;
     public static int sizeY = 600;
+    public static int titleSize = 1200;
 
     public Screen() {
         this.setSize(sizeX, sizeY);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
         this.setVisible(true);
     }
 
     public void initializeMainMenu() {
-        Label title = new Label(Main.gameTitle, 20);
+        Label title = new Label(Main.gameTitle, 40);
         this.add(title);
+        title.setBounds(250, -500, titleSize, titleSize);
     }
 }
